88748ab0ead9e12ee734a7a6b56333d658c53a06
ReyolJanu <ignatious.janukshan20@gmail.com>
2025-03-17 15:36:11 +0530

    setup DB 2

--- projectnext/app/models/postModels.js
+++ projectnext/app/models/postModels.js
@@ -4,6 +4,10 @@ const postSchema = new Schema({
     title:String,
     description:String,
     image:String
+}, { toJSON : {virtuals: true}});
+
+postSchema.virtual('short_description').get(function(){
+    return this.description.substr(0,100)+'...'
 })
 
 const PostModel = models.Post || model('post',postSchema);
--- projectnext/app/page.tsx
+++ projectnext/app/page.tsx
@@ -1,11 +1,13 @@
 "use client";
 import Image from "next/image";
+import Link from "next/link";
 import { useEffect, useState } from "react";
 
 interface Post {
   _id: string;
   title: string;
   description: string;
+  short_description:string;
   image?: string;
 }
 
@@ -32,11 +34,15 @@ export default function Home() {
 
       <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4">
         {posts.map((post) => (
+          <Link key={post._id} href={"/post/"+post._id}>
+
           <div key={post._id} className="border border-gray-200 p-4">
-            <img className="w-full h-48 object-cover mb-4" src={post.image || "https://picsum.photos/200"} alt="Post Image" />
+            <img className="w-full h-48 object-cover mb-4" src={post.image || "https://www.shutterstock.com/image-photo/bloggingblog-concepts-ideas-white-worktable-600nw-1029506242.jpg"} alt="Post Image" />
             <h2 className="text-xl font-semibold mb-2">{post.title}</h2>
-            <p className="text-gray-600">{post.description}</p>
+            <p className="text-gray-600">{post.short_description}</p>
           </div>
+
+          </Link>
         ))}
       </div>
     </div>
