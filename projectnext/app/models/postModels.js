6613e41a5d6f97d31007aa4f030280a4b4dd845f
ReyolJanu <ignatious.janukshan20@gmail.com>
2025-03-19 20:56:32 +0530

    DB fetching

--- projectnext/app/models/postModels.js
+++ projectnext/app/models/postModels.js
@@ -3,13 +3,24 @@ import { Schema,model,models } from "mongoose";
 const postSchema = new Schema({
     title:String,
     description:String,
-    image:String
+    image:String,
+    created_at:String
 }, { toJSON : {virtuals: true}});
 
 postSchema.virtual('short_description').get(function(){
     return this.description.substr(0,100)+'...'
-})
+});
 
-const PostModel = models.Post || model('post',postSchema);
+postSchema.virtual('created_at_formated').get(function(){
+    return changeDateFormate(this.created_at);
+});
+
+function changeDateFormate(date_str){
+    const date= new Date(date_str);
+    const months = ['January','February','March','April', 'May','June','July','August','September','October','November','December'];
+    return `${months[date.getMonth()]} ${date.getDate()}, ${date.getFullYear()}`;
+}
+
+const PostModel = models.Post || model('Post',postSchema);
 export default PostModel;
 
--- projectnext/app/post/[id]/page.js
+++ projectnext/app/post/[id]/page.js
@@ -1,13 +1,32 @@
-export default function Post(){
-    return(
-        <>
-        <main className="container mx-auto px-4 py-6">
-        <h2 className="text-4xl font-bold mb-4">Blog Post Title</h2>
-        <p className="text-gray-500">Published on January 1, 2022</p>
-        <img src="https://picsum.photos/200" alt="Post Image" className="my-4" />
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
-    </main>
-    </>
+"use client"
+import { useEffect, useState } from "react";
+import { use } from "react";
 
-    )
+export default function Post({ params }) {
+    const resolvedParams = use(params); // Unwrapping params
+    const id = resolvedParams.id;
+
+    const [post, setPost] = useState(null);
+
+    useEffect(() => {
+        // Only fetch if id has changed
+        if (id) {
+            fetch(`${process.env.NEXT_PUBLIC_API_URL}/post/${id}`)
+                .then(res => res.json())
+                .then(res => setPost(res));
+        }
+    }, [id]); // Adding id to the dependency array
+
+    return (
+        <>
+            {post && (
+                <main className="container mx-auto px-4 py-6">
+                    <h2 className="text-4xl font-bold mb-4">{post.title}</h2>
+                    <p className="text-gray-500">Published on {post.created_at_formated}</p>
+                    <img  src="https://picsum.photos/200" alt="Post Image" className="my-4 w-1/4 " />
+                    <p>{post.description}</p>
+                </main>
+            )}
+        </>
+    );
 }
