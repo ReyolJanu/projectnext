47af7965c4f5e6c8fce49ad0e754d9d96ff73f44
ReyolJanu <ignatious.janukshan20@gmail.com>
2025-03-20 20:24:49 +0530

    Fully Completed

--- projectnext/app/about/page.js
+++ projectnext/app/about/page.js
@@ -1,3 +1,8 @@
+export const metadata ={
+    title: "About Us Page",
+    description: "Janu Desined"
+}
+
 export default function About(){
     return(
         <h1>This is About Page!</h1>
---
+++ projectnext/app/models/enquiryModel.js
@@ -0,0 +1,19 @@
+const { Schema, model, models } = require("mongoose");
+
+const enquirySchema = new Schema({
+    name:{
+        type:String,
+        required:true
+    },
+    email:{
+        type:String,
+        required:true
+    },
+    message:{
+        type:String,
+        required:true
+    }
+})
+
+const EnquiryModel = models.Enquiry || model('Enquiry', enquirySchema);
+export default EnquiryModel;
